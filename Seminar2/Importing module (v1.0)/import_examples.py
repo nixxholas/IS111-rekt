313eedba8d0da95d02325bfc77e995ce89e07022
nixholas <nixholas@icloud.com>
2020-08-27 00:13:18 +0800

    Seminar2: Initial import, syntax fixes to notebook
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar2/Importing module (v1.0)/import_examples.py
@@ -0,0 +1,12 @@
+import greet
+name = input('Enter your name :')
+greet.say_hello(name)
+greet.say_bye(name)
+
+import math
+# prompt the user for a number
+num = float(input('Enter a number :'))
+# Calculate the square root of num
+result = math.sqrt(num) 
+# display the result
+print('Square root of',num,'is :',result)
---
+++ Seminar2/Importing module (v1.0)/main.py
@@ -0,0 +1,6 @@
+name = input('Enter your name :')
+greet.say_hello(name)
+greet.say_bye(name)
+
+
+
