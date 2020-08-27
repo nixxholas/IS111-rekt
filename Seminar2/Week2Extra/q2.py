6a3118cc0bec240ba1df10eb35ed32d34cd89c7b
nixholas <nixholas@icloud.com>
2020-08-27 11:25:03 +0800

    Seminar2: Week2Extra: q2: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar2/Week2Extra/q2.py
@@ -0,0 +1,19 @@
+def print_square(symbol, size):
+    '''
+    In Python, a string can be “multiplied by” an integer, which gives a new string that repeats the original string
+    multiple times. Try the following code to understand how this works:
+    '''
+    currLine = 0
+
+    # While current line does not exceed the size
+    while currLine < size:
+        # If we're at the first line or last line
+        if (currLine == 0) or ((currLine + 1) == size):
+            print(symbol * size)
+        else:
+            print(symbol + (" " * (size - 2)) + symbol)
+
+        currLine += 1;
+
+
+print_square(input("Enter the symbol of the square: "), int(input("Enter the size of the square: ")))
