b7521ff5c25d92cb4a770e530ef4c5f66acdd3be
nixholas <nixholas@icloud.com>
2020-08-27 11:29:03 +0800

    Seminar2: Week2Extra: Cleanup
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Week2Extra/q2.py
+++ Seminar2/Week2Extra/q2.py
@@ -1,3 +1,4 @@
+# Loop method
 def print_square(symbol, size):
     '''
     In Python, a string can be “multiplied by” an integer, which gives a new string that repeats the original string
@@ -13,7 +14,20 @@ def print_square(symbol, size):
         else:
             print(symbol + (" " * (size - 2)) + symbol)
 
-        currLine += 1;
+        currLine += 1
+
+# Recursion method
+def print_square_v2(symbol, size, currLine = 0):
+    if currLine < size:
+        # If we're at the first line or last line
+        if (currLine == 0) or ((currLine + 1) == size):
+            print(symbol * size)
+        else:
+            print(symbol + (" " * (size - 2)) + symbol)
+
+        currLine += 1
+        print_square_v2(symbol, size, currLine)
 
 
-print_square(input("Enter the symbol of the square: "), int(input("Enter the size of the square: ")))
+# print_square(input("Enter the symbol of the square: "), int(input("Enter the size of the square: ")))
+print_square_v2(input("Enter the symbol of the square: "), int(input("Enter the size of the square: ")))
