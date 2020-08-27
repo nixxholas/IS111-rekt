2be095aad0e54bcab8af674df384894ae5ab4d77
nixholas <nixholas@icloud.com>
2020-08-27 10:43:38 +0800

    Seminar2; Week 2 Starting code: q8a: done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Week2_Starting Code/q8a.py
+++ Seminar2/Week2_Starting Code/q8a.py
@@ -4,13 +4,14 @@ def calculate_tax_1(income):
     """
     This function assumes that the income is between $20,000 and $30,000.
     """
-    
+    # If u ain't earning 20-30k, get lost
+    if income < 20000 or income > 30000:
+        return Exception("Invalid income range.")
+
     # Modify the code below to return the right amount of tax.
-    
-    return 0.0
-    
-    
-    
+    return (income - 20000) * 0.02
+
+
 # ################################################################################
 
 # Call the function above to test whether it works.
