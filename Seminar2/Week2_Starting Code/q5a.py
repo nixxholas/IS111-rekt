5a0c80aae8bfd09531e82261527bdb6fdf2e62f8
nixholas <nixholas@icloud.com>
2020-08-27 10:29:47 +0800

    Seminar2: Week 2 starting code: q5: Split
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar2/Week2_Starting Code/q5a.py
@@ -0,0 +1,5 @@
+import math
+
+#q5a
+print("Each side of this square is", math.sqrt(float(input("What's the size of the square (in centimeters)? "))),
+      "centimeters.")
--- Seminar2/Week2_Starting Code/q5b.py
+++ Seminar2/Week2_Starting Code/q5b.py
@@ -1,9 +1,3 @@
-import math
-
-#q5a
-print("Each side of this square is", math.sqrt(float(input("What's the size of the square (in centimeters)? "))),
-      "centimeters.")
-
 import random
 
 #q5b
