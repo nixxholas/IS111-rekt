4906927bce9d939e2f3c143bdfae40e75bea2aaa
nixholas <nixholas@icloud.com>
2020-08-27 09:31:51 +0800

    Seminar2: Week2 Starting Code: q1b done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Week2_Starting Code/q1b.py
+++ Seminar2/Week2_Starting Code/q1b.py
@@ -5,12 +5,10 @@ def compute_geometric_mean(x, y, z):
     This function returns the geometric mean of the three numbers x, y and z.
     """
     # Write your code here:
-    
-    
-    
-    
+    return (x * y * z) ** (1. / 3.)
 
-# ################################################################################    
+
+# ################################################################################
 # The code below is to test your implementation above.
 # DO NOT MODIFY THE CODE BELOW!
 
