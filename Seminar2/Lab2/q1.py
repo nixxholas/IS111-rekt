9f988e7ffc7eb592922764ed9cda6c225f08f85b
nixholas <nixholas@icloud.com>
2020-08-28 11:22:11 +0800

    Seminar2: Lab2: q1: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Lab2/q1.py
+++ Seminar2/Lab2/q1.py
@@ -18,16 +18,21 @@ def perform_magic_4(z):
     return (z + 1)
 
 x = 0
+# 0
 print(x)
 
 perform_magic_1(x)
+# 0, no proper return
 print(x)
 
+# 0, no proper setting, x = perform_magic_2 is required
 perform_magic_2(x)
 print(x)
 
+# 3
 x = perform_magic_3(x)
 print(x)
 
+# 15
 x = perform_magic_4(x)
 print(x)
