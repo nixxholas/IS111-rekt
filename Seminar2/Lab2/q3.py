6295e368226587b60a68cec358952a04e13148a6
nixholas <nixholas@icloud.com>
2020-08-28 14:20:30 +0800

    Seminar2: Lab2: q3: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Lab2/q3.py
+++ Seminar2/Lab2/q3.py
@@ -22,5 +22,14 @@ def print_pattern(my_str, delimiter_str, n):
 ########################################################
 # Write your code below to print out the correct output.
 
+# help(print_pattern)
+space=' ';
 
-
+print_pattern("SMU", " * ", 10)
+print_pattern("* PYTHON *", "*", 10)
+print()
+print_pattern("*", space, 4)
+print_pattern(space + "*", "", 3)
+print_pattern("*", space, 4)
+print_pattern(space + "*", "", 3)
+print_pattern("*", space, 4)
