0c82dc78115839642454d9d9a9629711ea0b0f41
nixholas <nixholas@icloud.com>
2020-08-29 13:42:06 +0800

    Seminar2: Lab2: q5: Output was wrong
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Lab2/q5.py
+++ Seminar2/Lab2/q5.py
@@ -15,8 +15,12 @@ def calculate_price_after_discount(unit_price, quantity, discount):
 # ########################################
 # lab2_Q5_part2: Write your code below:
 
-print("Ruler:", calculate_price_after_discount(1.5, 5, 10))
-print("Milk:", calculate_price_after_discount(5.95, 2, 10))
-print("Rice:", calculate_price_after_discount(6.50, 1, 5))
-print("Eggs:", calculate_price_after_discount(2.4, 2, 0))
-print("Kaya:", calculate_price_after_discount(3.95, 3, 15))
+total = 0.0
+total = total + calculate_price_after_discount(5.95, 2, 10) + calculate_price_after_discount(6.50, 1, 5) \
+        + calculate_price_after_discount(2.4, 2, 0) + calculate_price_after_discount(3.95, 3, 15)
+print("The total of your shopping cart after discount is $" + str(total))
+# print("Ruler:", calculate_price_after_discount(1.5, 5, 10))
+# print("Milk:", calculate_price_after_discount(5.95, 2, 10))
+# print("Rice:", calculate_price_after_discount(6.50, 1, 5))
+# print("Eggs:", calculate_price_after_discount(2.4, 2, 0))
+# print("Kaya:", calculate_price_after_discount(3.95, 3, 15))
