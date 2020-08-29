da96b2c94207b63e4966933f877cf062f425cbc6
nixholas <nixholas@icloud.com>
2020-08-29 12:47:55 +0800

    Seminar2: Lab2: q4: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Lab2/q4.py
+++ Seminar2/Lab2/q4.py
@@ -1,10 +1,10 @@
-#Lab2_Q4
+# Lab2_Q4
 # #####################################
 # Write your code below to first define 
 # the function calculate_interest()
 
 def calculate_interest(principal, annual_interest_rate, frequency_of_compounding, deposit_rate):
-    return 0
+    return (principal * (1.0 + annual_interest_rate / frequency_of_compounding) ** (deposit_rate * 12)) - principal
 
 
 # ################################################################
@@ -20,9 +20,7 @@ FREQUENCY_OF_COMPOUNDING = 12
 # Write your code below to prompt the user and display the 
 # interest earned.
 
-print("The interest you will earn is", calculate_interest(float(input("What's the amount of your principal? "),
-                                                                ANNUAL_INTEREST_RATE, FREQUENCY_OF_COMPOUNDING,
-                                                                float(input("How many years do you want to deposit "
-                                                                            "the money? ")))))
-
-
+print("The interest you will earn is", calculate_interest(float(input("What's the amount of your principal? ")),
+                                                          ANNUAL_INTEREST_RATE, FREQUENCY_OF_COMPOUNDING,
+                                                          float(input("How many years do you want to deposit "
+                                                                      "the money? "))))
