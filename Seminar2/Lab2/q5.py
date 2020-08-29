673941a983de7fc0bb91c7d238eed6f4eb792e9e
nixholas <nixholas@icloud.com>
2020-08-29 13:39:49 +0800

    Seminar2: Lab2: q5: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar2/Lab2/q5.py
+++ Seminar2/Lab2/q5.py
@@ -1,20 +1,22 @@
 # Lab2_Q5
 # ########################################
 # # lab2_Q5_part1: Write your code below:
+def calculate_price_after_discount(unit_price, quantity, discount):
+    """
+    unit_price: The unit price of an item
+    quantity: The quantity of that item
+    discount: The discount rate offered by the store for that item
 
-
-
-
-
-
-
+    returns: The discount rate offered by the store for that item
+    """
+    return unit_price * quantity * (1 - discount / 100)
 
 
 # ########################################
 # lab2_Q5_part2: Write your code below:
 
-
-
-
-
-
+print("Ruler:", calculate_price_after_discount(1.5, 5, 10))
+print("Milk:", calculate_price_after_discount(5.95, 2, 10))
+print("Rice:", calculate_price_after_discount(6.50, 1, 5))
+print("Eggs:", calculate_price_after_discount(2.4, 2, 0))
+print("Kaya:", calculate_price_after_discount(3.95, 3, 15))
