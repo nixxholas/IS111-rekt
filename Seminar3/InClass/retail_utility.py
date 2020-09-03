7ad4226a9061cdf8ae5770de3e572d35297b577b
nixholas <nixholas@icloud.com>
2020-09-03 10:19:25 +0800

    Seminar4: InClass: q5i: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar3/InClass/retail_utility.py
@@ -0,0 +1,5 @@
+def calculate_max_quantity_and_change(unit_price, amount):
+    return amount // unit_price, amount - ((amount // unit_price) * unit_price)
+
+
+print(calculate_max_quantity_and_change(58.5, 130.0))
