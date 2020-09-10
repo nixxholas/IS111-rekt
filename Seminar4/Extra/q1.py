22eac1c4de8d6f7f26e2d0d7d403348a198c8c6b
nixholas <nixholas@icloud.com>
2020-09-10 11:03:32 +0800

    Seminar4: Extra: q1: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar4/Extra/q1.py
+++ Seminar4/Extra/q1.py
@@ -1,6 +1,31 @@
 import math
 
 
+def print_square_2(n):  # n is a +ve odd number >= 5
+    mid = math.floor(n / 2)
+
+    for j in range(0, n):  # Loop vertically
+        # Print horizontally
+        # The first or the last is always a complete print.
+        if j == 0 or j == (n - 1):
+            for i in range(0, n):
+                if i == (n - 1):
+                    print("*")
+                else:
+                    print("*", end='')
+        else:
+            for i in range(0, n):
+                if i == 0:
+                    print("*", end='')
+                elif i == (n - 1):
+                    print("*")
+                else:
+                    if i == j or j == (n - 1) - i:
+                        print("*", end='')
+                    else:
+                        print(" ", end='')
+
+
 def print_square_1(n):  # n is a +ve odd number >= 5
     # Prints out an n x n square using asterisks that's divided into four small squares
     mid = math.floor(n / 2)
@@ -8,8 +33,10 @@ def print_square_1(n):  # n is a +ve odd number >= 5
         # Print horizontally
         if j == 0 or j == (n - 1) or j == mid:
             for i in range(0, n):
-                if i == (n - 1): print("*")
-                else: print("*", end='')
+                if i == (n - 1):
+                    print("*")
+                else:
+                    print("*", end='')
         else:
             for i in range(0, n):
                 if i == 0 or i == mid:
@@ -20,4 +47,5 @@ def print_square_1(n):  # n is a +ve odd number >= 5
                     print(" ", end='')
 
 
-print_square_1(int(input("Enter the size of the square: ")))
+# print_square_1(int(input("Enter the size of the square: ")))
+print_square_2(int(input("Enter the size of the square: ")))
