dcf4ed0a16e10039cd401ac2ce985c96b91d3a79
nixholas <nixholas@icloud.com>
2020-09-10 12:33:50 +0800

    Seminar4: Lab4: q4: Missed out on part 2!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar4/Lab4/q4.py
+++ Seminar4/Lab4/q4.py
@@ -6,4 +6,12 @@ def encrypt(msg):
     return res
 
 
-print("The encrypted message is '" + encrypt(input("What's the original message? ")) + "'")
+def encrypt_v2(msg):
+    res = ''
+    for char in msg:  # One liner .replace will not work, since we have overlapping replacements.
+        res += 'e' if char == 'a' else 'i' if char == 'e' else 'o' if char == 'i' else 'u' if char == 'o' else 'a' \
+            if char == 'u' else char
+    return res[::-1]
+
+
+print("The encrypted message is '" + encrypt_v2(input("What's the original message? ")) + "'")
