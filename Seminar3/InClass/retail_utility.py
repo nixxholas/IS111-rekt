5e46de3445544b80924dc3de79d39ad94e55b7cb
nixholas <nixholas@icloud.com>
2020-09-10 08:37:06 +0800

    Seminar3: InClass: Finalisation
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar3/InClass/retail_utility.py
+++ Seminar3/InClass/retail_utility.py
@@ -1,5 +1,4 @@
 def calculate_max_quantity_and_change(unit_price, amount):
-    return amount // unit_price, amount - ((amount // unit_price) * unit_price)
-
+    return int(amount // unit_price), amount - ((amount // unit_price) * unit_price)
 
 # print(calculate_max_quantity_and_change(58.5, 130.0))
