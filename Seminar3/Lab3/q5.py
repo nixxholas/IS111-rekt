26ec7ef9264cf481d46e1e4e2ae44cc4b46f0f56
nixholas <nixholas@icloud.com>
2020-09-10 09:11:31 +0800

    Seminar3: Lab3: q5: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar3/Lab3/q5.py
+++ Seminar3/Lab3/q5.py
@@ -14,8 +14,15 @@ def get_user_info():
     is_student = input("Are you a student? [yes|no] ")
     return (name, gender, age, is_student == 'yes')
 
+
 # Write your code below:
-
-
-
-
+user_info = get_user_info()
+if user_info[2] >= 60:
+    print(("Mr. " if user_info[1] == 'M' else "Ms. ") + user_info[0] + ", you can get concessionary fare for senior "
+                                                                       "citizens.")
+elif user_info[2] <= 6:
+    print(user_info[0] + ", you can get travel for free.")
+elif user_info[3]:
+    print("Mr. " if user_info[1] == 'M' else "Ms. " + user_info[0] + ", you can get concessionary fare for students.")
+else:
+    print("Mr. " if user_info[1] == 'M' else "Ms. " + user_info[0] + ", you need to pay full fare.")
