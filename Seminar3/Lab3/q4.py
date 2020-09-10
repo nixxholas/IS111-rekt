d0bba451d4a4b2327740a0310bce854e70aca05f
nixholas <nixholas@icloud.com>
2020-09-10 09:00:56 +0800

    Seminar3: Lab3: q4: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar3/Lab3/q4.py
+++ Seminar3/Lab3/q4.py
@@ -2,16 +2,10 @@
 # PART 1
 # The following function is for you to implement.
 def get_ticket_info(age):
-    
     # Modify the code below to return the right value
-    
-    
+    return (15, True) if (age >= 60) else (33, False) if (age >= 13) else (22, True)
 
-    
-    
-    return (0, False)
-    
-    
+
 # ################################################################################
 # The code below is to test your implementation of the function above. 
 # DO NOT MODIFY THE CODE BELOW!
@@ -22,11 +16,10 @@ print(get_ticket_info(40))
 # The following line of code should display (22, True)
 print(get_ticket_info(10))
 
-
 # PART 2
 # Write your code below to prompt the user for age
 # and print the expected output
-
-
-
-
+age = int(input("How old are you? "))
+t_info = get_ticket_info(age)
+if t_info[1]: print("Congratulations! You qualify for a discount.")
+print("You need to pay $" + str(t_info[0]))
