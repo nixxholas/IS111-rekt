5882c1fe41b47af8d5fd9c1d90544473b962b99e
nixholas <nixholas@icloud.com>
2020-09-10 08:47:22 +0800

    Seminar3: Lab3: q1: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar3/Lab3/q1a.py
+++ Seminar3/Lab3/q1a.py
@@ -1,24 +1,27 @@
 # Q1a
 def perform_magic1(condition, output):
-    if (condition):
+    if (condition):  # False
         print(output)
 
+
 def perform_magic2(condition, output):
-    if (condition):
+    if (condition):  # False
         print(output)
     else:
         print("The condition is false!")
 
+
 def perform_magic3(a, b):
-    if (a >= b):
+    if (a >= b):  # True
         print("a >= b")
- 
-    if (a <= b):
+
+    if (a <= b): # True
         print("a <= b")
+
 
 a = 12
 b = 15
 
-perform_magic1(a == b, "a == b")
-perform_magic2(a > b, "a > b")
-perform_magic3(a + 3, b)
+perform_magic1(a == b, "a == b")  # perform_magic1 won't print any output
+perform_magic2(a > b, "a > b")  # prints 'The condition is false!'
+perform_magic3(a + 3, b)  # Passes all conditions
