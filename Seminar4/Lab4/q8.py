8566891df0c33ef6dfca089883d7de70ff934a26
nixholas <nixholas@icloud.com>
2020-09-10 23:28:42 +0800

    Seminar4: Lab4: q8: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar4/Lab4/q8.py
+++ Seminar4/Lab4/q8.py
@@ -1,13 +1,16 @@
 ## Q8
 # ################################################################################
-# The function below is for you to implement! 
+# The function below is for you to implement!
 def display_fibonacci(n):
     """
     This function takes in an integer n (greater or equal to 3). It prints out the 
     first n Fibonacci numbers, starting from 1. The function doesn’t return anything.
     """
     # Modify the code below to print the first n Fibonacci numbers
-    
-
-
-
+    for i in range(1, n + 1):
+        # print("(" + str(i) + ")", end= ' ')
+        if i <= 1:
+            print(i, end=' ')
+        else:
+            print(i - 1 + i - 2, end=' ')
+    print()
