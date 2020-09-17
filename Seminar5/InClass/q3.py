89af705790b3e881bf328e032d05aec5e4084b20
nixholas <nixholas@icloud.com>
2020-09-18 01:12:43 +0800

    Seminar5: InClass: q3: Hotfix
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/InClass/q3.py
+++ Seminar5/InClass/q3.py
@@ -1,8 +1,9 @@
 def is_valid_username(username):
-    return False if ((' ' in username) or (username is '') or (len(username) > 8)
-                     or any(x.isupper() for x in username)) else True
+    # Ideally, we would want to stash all UTF-approved characters in a list for a precise and succinct check.
+    return False if ((' ' in username) or (':' in username) or ('-' in username) or (username is '')
+                     or (len(username) > 8) or any(x.isupper() for x in username)) else True
 
 
-username_list = ['ab{cdefgh', 'abcdefghi', 'ab$cd', 'ab_cd', 'ab-cd', 'ab:cd', '', 'ab cd', 'abcDef', 'abc8ef']
+username_list = ['abcdefgh', 'abcdefghi', 'ab$cd', 'ab_cd', 'ab-cd', 'ab:cd', '', 'ab cd', 'abcDef', 'abc8ef']
 for username in username_list:
     print("The username '" + username + "' is valid : " + str(is_valid_username(username)))
