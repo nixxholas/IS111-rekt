f1e0848b48076565cb4dec0ce35e0f22b69b32a6
nixholas <nixholas@icloud.com>
2020-09-17 11:05:44 +0800

    Seminar5: Lab5: q2a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/Lab5/q2a.py
+++ Seminar5/Lab5/q2a.py
@@ -2,15 +2,14 @@
 ## a) 
 # Write your code below:
 ##############################################################
+def get_leap_years(years):
+    res = []
+    for year in years:
+        if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0): res.append(year)
+    return res
 
 
-
-
-
-
-
-
-##############################################################
+    ##############################################################
 # Test Cases to test your code
 # DO NOT MODIFY THE TEST CODES
 
