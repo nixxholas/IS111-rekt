7becbf42de49f1c54ee8d5e535b1951d8cad2fd4
nixholas <nixholas@icloud.com>
2020-09-17 10:37:40 +0800

    Seminar5: Lab5: q6a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/Lab5/q6a.py
+++ Seminar5/Lab5/q6a.py
@@ -2,11 +2,22 @@
 ## a)
 # Write your code below:
 ##############################################################
+def get_all_combinations(str_list, num_list):
+    """
+    Takes in two lists, str_list containing a sequence of strings and num_list containing a sequence of integers,
+    with both lists containing different sizes.
 
+    Returns a list of tuples.
 
+    For example, get_all_combinations(["a", "b"], [1, 2, 3]) should return
+    [("a", 1), ("a", 2), ("a", 3), ("b", 1), ("b", 2), ("b", 3)].
+    """
+    res = []
+    for s in str_list:
+        for n in num_list:
+            res.append((s, n))
 
-
-
+    return res
 
 
 ##############################################################
