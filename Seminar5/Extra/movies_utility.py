45d6dcd273d355aeafb8a42b36357af557d4d795
nixholas <nixholas@icloud.com>
2020-09-19 02:57:21 +0800

    Seminar5: Extra: movies_utility: get_movies_with_keyword: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/Extra/movies_utility.py
+++ Seminar5/Extra/movies_utility.py
@@ -15,3 +15,11 @@ def get_title_of_longest_movie(movie_list, longest_title='', duration=0):
             longest_title = movie_list[-1][0]
             duration = movie_list[-1][2]
         return get_title_of_longest_movie(movie_list[:-1], longest_title, duration)
+
+
+def get_movies_with_keyword(movie_list, keyword):
+    movie_list_with_keyword = []
+    for i in range(0, len(movie_list)):
+        if keyword in movie_list[i][0]: movie_list_with_keyword.append(movie_list[i])
+
+    return movie_list_with_keyword
