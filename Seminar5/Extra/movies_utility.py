2902e80e70ef48775c800148bef0f01700f90450
nixholas <nixholas@icloud.com>
2020-09-19 02:45:03 +0800

    Seminar5: Extra: movies_utility: get_num_movies_of_genre: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/Extra/movies_utility.py
+++ Seminar5/Extra/movies_utility.py
@@ -1,2 +1,7 @@
 def get_average_duration(movie_list):
     return 0.0 if len(movie_list) == 0 else (sum(m[2] for m in movie_list) / len(movie_list))
+
+
+def get_num_movies_of_genre(movie_list, genre, count=0):
+    if len(movie_list) > 0 and movie_list[-1][1] == genre: count += 1
+    return count if len(movie_list) == 0 else get_num_movies_of_genre(movie_list[:-1], genre, count)
