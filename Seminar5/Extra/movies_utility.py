5f22d7a0eee8913ddde6d6622da50caf39bb67e1
nixholas <nixholas@icloud.com>
2020-09-19 02:53:24 +0800

    Seminar5: Extra: movies_utility: get_title_of_longest_movie: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar5/Extra/movies_utility.py
+++ Seminar5/Extra/movies_utility.py
@@ -5,3 +5,13 @@ def get_average_duration(movie_list):
 def get_num_movies_of_genre(movie_list, genre, count=0):
     if len(movie_list) > 0 and movie_list[-1][1] == genre: count += 1
     return count if len(movie_list) == 0 else get_num_movies_of_genre(movie_list[:-1], genre, count)
+
+
+def get_title_of_longest_movie(movie_list, longest_title='', duration=0):
+    if len(movie_list) == 0:
+        return longest_title
+    else:
+        if movie_list[-1][2] > duration:
+            longest_title = movie_list[-1][0]
+            duration = movie_list[-1][2]
+        return get_title_of_longest_movie(movie_list[:-1], longest_title, duration)
