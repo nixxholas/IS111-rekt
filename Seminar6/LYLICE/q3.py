093d3b3c844e2b8391e9fa001633b5b97e2bdc30
nixholas <nixholas@icloud.com>
2020-09-22 16:23:15 +0800

    Seminar6: LYLICE: q3: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar6/LYLICE/q3.py
@@ -0,0 +1,14 @@
+# def print_expanded(n, res=''):
+#     print(str(n) + ' + ' + res) if n % 10 <= 0 else print_expanded(n % 10, str(n - n % 10) + ' + ' + res)
+
+def print_expanded(n):
+    digits = list(str(n))
+    res = ''
+
+    for i in range(0, len(digits)):
+        if int(digits[i]) > 0:
+            res += (str(digits[i]) if (len(digits) - 1) == i else str(digits[i]) + ('0' * (len(digits) - (1 + i))) + ' + ')
+    print(res)
+
+
+print_expanded(100003)
