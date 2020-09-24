5ff109d3cc0c6bd91c8877cfc4e584211a35dd0a
nixholas <nixholas@icloud.com>
2020-09-24 09:27:58 +0800

    Seminar6: InClass: q1: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar6/InClass/q1.py
+++ Seminar6/InClass/q1.py
@@ -1,1 +1,14 @@
+from statistics import mean
+
+
 def get_larger_values(values):
+    avg = mean(values)
+
+    for v in values:
+        if v < avg:
+            values.remove(v)
+
+    return values
+
+
+print(get_larger_values([2.5, 3.5, 5.5, 1.0]))
