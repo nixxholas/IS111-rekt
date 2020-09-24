46944ef50425520119618fd38a16c59b1e926958
nixholas <nixholas@icloud.com>
2020-09-24 23:56:49 +0800

    LabTest: 1920: q4a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q4a.py
+++ LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q4a.py
@@ -1,7 +1,31 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen
+# Email ID: hwchen.2020
 
 def check_seating_arrangement(arrangement, must_list, cannot_list):
-    # Modify the code below.
-    return None
+    # Check the must list first.
+    if len(must_list) > 0:
+        # For every tuple in must_list
+        for t in must_list:
+            # Ensure they sit beside each other - standard index method
+            if (arrangement.index(t[0]) - arrangement.index(t[1])) == 1 or (
+                    arrangement.index(t[0]) - arrangement.index(t[1])) == -1 or (
+                    # Perform a 'on the edge' scenario where they sit at index 0 and last index
+                    arrangement.index(t[0]) - arrangement.index(t[1]) == (len(arrangement) - 1)) or (
+                    arrangement.index(t[0]) - arrangement.index(t[1]) == -(len(arrangement) - 1)): continue
+            else:
+                print(t)
+                return False
+
+    # Check the cannot list first.
+    if len(cannot_list) > 0:
+        # For every tuple in cannot_list
+        for t in cannot_list:
+            # Ensure they do NOT sit beside each other - standard index method
+            if (arrangement.index(t[0]) - arrangement.index(t[1])) == 1 or (
+                    arrangement.index(t[0]) - arrangement.index(t[1])) == -1 or (
+                    # Perform a 'on the edge' scenario where they do NOT sit at index 0 and last index
+                    arrangement.index(t[0]) - arrangement.index(t[1]) == (len(arrangement) - 1)) or (
+                    arrangement.index(t[0]) - arrangement.index(t[1]) == -(len(arrangement) - 1)): return False
+
+    return True
  
