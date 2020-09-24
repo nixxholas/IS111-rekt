eea07c564a81a8c4fbef1ec6187b2290119d87de
nixholas <nixholas@icloud.com>
2020-09-24 23:26:29 +0800

    LabTest: 1920: q2a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q2a.py
+++ LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q2a.py
@@ -1,6 +1,8 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen
+# Email ID: hwchen.2020
 
 def get_number_of_long_strings(str_list, n):
-    # Modify the code below.
-    return None
+    count = 0
+    for s in str_list:
+        if len(s) > n: count += 1
+    return count
