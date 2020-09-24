ff6d259c5a0cf34739e91451ac0a82378334a1a5
nixholas <nixholas@icloud.com>
2020-09-24 09:54:22 +0800

    Seminar6: InClass: movies_utility: part 2 done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar6/InClass/movies_utility.py
+++ Seminar6/InClass/movies_utility.py
@@ -6,3 +6,12 @@ def get_average_duration(movie_list):
         total_duration += m[2]
 
     return total_duration / len(movie_list)
+
+
+def get_num_movies_of_genre(movie_list, genre):
+    count = 0
+
+    for m in movie_list:
+        if m[1] == genre: count += 1
+
+    return count
