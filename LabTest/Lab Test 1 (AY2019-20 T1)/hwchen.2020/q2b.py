b4f66ce1034d929ff1913f968f505223cce984bb
nixholas <nixholas@icloud.com>
2020-09-24 23:29:27 +0800

    LabTest: 1920: q2b: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q2b.py
+++ LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q2b.py
@@ -1,6 +1,13 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen
+# Email ID: hwchen.2020
 
 def add_even_numbers(str_list):
-    # Modify the code below.
-    return None
+    res = 0
+    for s in str_list:
+        # Split the strings first
+        num_strings = s.split('|')
+        # Then loop the numbers and find even numbers to find the result.
+        for num_string in num_strings:
+            if int(num_string) % 2 == 0: res += int(num_string)
+
+    return res
