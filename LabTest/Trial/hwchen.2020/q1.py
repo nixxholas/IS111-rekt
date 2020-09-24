9f59849fc06ba88018e85d6e39a573f6541f6e21
nixholas <nixholas@icloud.com>
2020-09-24 09:05:55 +0800

    LabTest: Trial: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Trial/hwchen.2020/q1.py
+++ LabTest/Trial/hwchen.2020/q1.py
@@ -1,9 +1,20 @@
 # =====
 # q1.py
 # =====
-# Name: 
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def add_first_odd_digits(str_list):
-    # modify the code below
-    return 0
+    odd_digits = ['1', '3', '5', '7', '9']
+    val = 0
+    for word in str_list:
+        chars = list(word)
+
+        while len(chars) > 0:
+            c = chars.pop(0)
+
+            if c in odd_digits:
+                val += int(c)
+                chars = []
+
+    return val
--- LabTest/Trial/hwchen.2020/q2.py
+++ LabTest/Trial/hwchen.2020/q2.py
@@ -1,11 +1,26 @@
 # =====
 # q2.py
 # =====
-# Name: 
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def display_strings(str_list, ch):
-    
-    # Modify the code below
-    
+    max_length = 0
+
+    # Find the longest
+    for phrase in str_list:
+        if len(phrase) > max_length:
+            max_length = len(phrase)
+
+    # Set the '-'
+    for i in range(len(str_list)):
+        if len(str_list[i]) < max_length:
+            str_list[i] += (ch * (max_length - len(str_list[i])))
+
+    # Print the strings
+    height = len(str_list) - 1
+    while len(str_list) > 0:
+        print((' ' * height) + str_list.pop(0))
+        height -= 1
+
     return
