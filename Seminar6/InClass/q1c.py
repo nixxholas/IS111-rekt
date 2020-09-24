c3e323b1ff94c8cfd845fc79dbff41a161dd1796
nixholas <nixholas@icloud.com>
2020-09-24 09:36:10 +0800

    Seminar6: InClass: q1c: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar6/InClass/q1c.py
@@ -0,0 +1,14 @@
+def check_numbers(list1, list2):
+    divisible = True
+    for i in list1:
+        i_divisible = False
+        for j in list2:
+            if i % j == 0: i_divisible = True
+        divisible = i_divisible
+
+    return divisible
+
+
+print(check_numbers([3, 8, 10, 15, 16], [9, 3, 2, 5]))
+print(check_numbers([3, 8, 10, 6, 2, 5], [9, 3, 7]))
+
