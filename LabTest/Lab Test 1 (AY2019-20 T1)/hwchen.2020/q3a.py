f88100c4c558627a93f5fd315cba96e224d75fbe
nixholas <nixholas@icloud.com>
2020-09-24 23:35:14 +0800

    LabTest: 1920: q3a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q3a.py
+++ LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q3a.py
@@ -1,12 +1,22 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen
+# Email ID: hwchen.2020
 
 def calculate_entrance_fees_1(n):
-
     # These variables are defined for you to use.
     PACKAGE_B = 110
     PACKAGE_C = 200
 
     # Modify the code below.
-    return None
-        
+    total = 0
+
+    if n <= 1: return n * PACKAGE_B
+
+    while n > 0:
+        if n - 2 >= 0:
+            total += PACKAGE_C
+            n -= 2
+        elif n - 1 >= 0:
+            total += PACKAGE_B
+            n -= 1
+
+    return total
