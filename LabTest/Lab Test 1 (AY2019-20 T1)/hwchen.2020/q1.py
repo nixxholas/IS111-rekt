4a4016a9063d46ffacd6316dd4742b6cbcd09cb5
nixholas <nixholas@icloud.com>
2020-09-24 23:24:36 +0800

    LabTest: 1920: q1: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q1.py
+++ LabTest/Lab Test 1 (AY2019-20 T1)/hwchen.2020/q1.py
@@ -1,7 +1,5 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen
+# Email ID: hwchen.2020
 
 def check_sum(n1, n2, n3):
-    # Modify the code below.
-    return None
-    
+    return True if (n1 == (n2 + n3)) or (n2 == (n1 + n3)) or (n3 == (n1 + n2)) else False
