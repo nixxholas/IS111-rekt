f30a2419951a9c1aa85625fda2a2c33886622598
nixholas <nixholas@icloud.com>
2020-09-26 11:29:52 +0800

    LabTest: Actual: LT1: q3: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Actual/LT1_student_resources/hwchen.2020/q3.py
+++ LabTest/Actual/LT1_student_resources/hwchen.2020/q3.py
@@ -27,21 +27,36 @@ def check_math(list_of_equations):
                     # If the equation symbol is not found yet, we still tryna fill up the number.
                     if eqn_symbol is None:
                         first_val = int(c) if first_val is None else int(str(first_val) + c)
+                    # If the equal symbol is not found yet, we still tryna fill up the number.
                     elif not equal_found:
                         second_val = int(c) if second_val is None else int(str(second_val) + c)
+                    # If the equation is not ending yet, we still tryna fill up the number.
                     else:
                         final_res = int(c) if final_res is None else int(str(final_res) + c)
                 elif not c.isnumeric() and c not in string.ascii_letters:
+                    # Since its not numeric and not letter-based, its most likely a symbol
                     if eqn_symbol is None and c != "=":
+                        # Make sure its not = so that we can assume its an arithmetic
+                        # operation with a much higher precision.
                         eqn_symbol = c
                     elif c != "=" and len(eqn_symbol) < 2:
+                        # Since eqn_symbol may be a //, we let it in if its not an =
                         eqn_symbol += c
-                    elif c == "=":
+                    elif c == "=" and not equal_found:
+                        # Since this is an =, we sort this out as an equal found.
                         equal_found = True
+                    else:
+                        # If we have a duplicate symbol or something, we terminate immediately.
+                        return False
 
+        # If any of the arithmetic conditions are not met,
         if first_val is None or second_val is None or eqn_symbol is None or final_res is None:
             return False
         else:
+            # Else we can safely assume its more or less an arithmetic operation
+            # Condition 1: Ensure its a proper arithmetic operation
+            # Condition 2: Ensure the operation always == final_res
+            # If these are not met, return False and stop immediately, no need to continue.
             if eqn_symbol == "*":
                 if (first_val * second_val) != final_res:
                     return False
@@ -60,6 +75,7 @@ def check_math(list_of_equations):
             elif eqn_symbol == "-":
                 if (first_val - second_val) != final_res:
                     return False
+            # Invalid symbol, don't test my patience lol.
             else:
                 return False
 
