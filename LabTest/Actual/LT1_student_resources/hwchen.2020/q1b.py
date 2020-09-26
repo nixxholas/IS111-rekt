1a267e0219dc9ae0989a979704a9ff21f655522e
nixholas <nixholas@icloud.com>
2020-09-26 10:06:28 +0800

    LabTest: Actual: LT1: q1b: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Actual/LT1_student_resources/hwchen.2020/q1b.py
+++ LabTest/Actual/LT1_student_resources/hwchen.2020/q1b.py
@@ -1,6 +1,10 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def is_compatible(patient_group, donor_group):
-    # Replace the code below with your implementation.
-    return None
+    return True if (patient_group == 'A' and (donor_group == "A" or donor_group == "AB")) or (
+            patient_group == 'B' and (donor_group == "B" or donor_group == "AB")) or (
+                           patient_group == 'AB' and donor_group == "AB") or (
+                           patient_group == 'O' and (
+                           donor_group == "O" or donor_group == "AB" or donor_group == "A" or donor_group == "B")) \
+        else False
