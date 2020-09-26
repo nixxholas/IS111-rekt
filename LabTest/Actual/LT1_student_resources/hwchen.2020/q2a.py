dbabf4f55b29f89646f11d8f193f04b0f18bde6b
nixholas <nixholas@icloud.com>
2020-09-26 10:07:57 +0800

    LabTest: Actual: LT1: q2a: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Actual/LT1_student_resources/hwchen.2020/q2a.py
+++ LabTest/Actual/LT1_student_resources/hwchen.2020/q2a.py
@@ -1,6 +1,10 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def get_sum_of_digits(my_str):
-    # Replace the code below with your implementation.
-    return None
+    total = 0
+
+    for c in my_str:
+        if c.isnumeric(): total += int(c)
+
+    return total
