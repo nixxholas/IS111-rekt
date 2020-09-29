76d0d92b406dc8f802ffd0a9c05dd00e31de0b0c
nixholas <nixholas@icloud.com>
2020-09-30 03:29:35 +0800

    Seminar7: InClass: WebApplication: Proper import
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar7/InClass/WebApplication/util.py
@@ -0,0 +1,30 @@
+def get_file_extension(file_name):
+    return ''
+
+def is_image_file(file_name):
+    return  'cat-1' in file_name 
+
+def is_square(dimension):
+    return True
+
+def is_portrait(dimension):
+    return True
+
+def is_landscape(dimension):
+    return True
+
+def get_square_image_details(image_details):
+    return image_details
+
+def get_portrait_image_details(image_details):
+    return []
+
+def get_landscape_image_details(image_details):
+    return []
+
+def get_matching_files(file_list, keyword):
+    return []
+
+
+
+
