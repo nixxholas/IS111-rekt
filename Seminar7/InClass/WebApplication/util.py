0489cc413f5d178f991319f1b7b950f4606cca8c
nixholas <nixholas@icloud.com>
2020-09-30 03:41:57 +0800

    Seminar7: InClass: WebApplication: util: Grouping function fixes
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar7/InClass/WebApplication/util.py
+++ Seminar7/InClass/WebApplication/util.py
@@ -5,22 +5,37 @@ def is_image_file(file_name):
     return get_file_extension(file_name).lower() in {'png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif'}
 
 def is_square(dimension):
-    return True
+    return dimension[0] == dimension[1]
 
 def is_portrait(dimension):
-    return True
+    return dimension[1] > dimension[0]
 
 def is_landscape(dimension):
-    return True
+    return dimension[0] > dimension[1]
 
 def get_square_image_details(image_details):
-    return image_details
+    images = []
+
+    for i in image_details:
+        if is_square(i[1]): images.append(i)
+
+    return images
 
 def get_portrait_image_details(image_details):
-    return []
+    images = []
+
+    for i in image_details:
+        if is_portrait(i[1]): images.append(i)
+
+    return images
 
 def get_landscape_image_details(image_details):
-    return []
+    images = []
+
+    for i in image_details:
+        if is_landscape(i[1]): images.append(i)
+
+    return images
 
 def get_matching_files(file_list, keyword):
     return []
