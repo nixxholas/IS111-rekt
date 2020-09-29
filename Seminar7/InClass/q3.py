fb503837115a234e510f8d0fd4e147c1a8cde08f
nixholas <nixholas@icloud.com>
2020-09-30 03:16:04 +0800

    Seminar7: InClass: q3: Done!!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar7/InClass/q3.py
+++ Seminar7/InClass/q3.py
@@ -7,3 +7,12 @@ def print_triangle(ch, num_rows):
         print((' ' * spacing) + (ch * chars))
         spacing -= 1
         chars += 2
+
+
+def print_frame(ch, num_rows, num_cols):
+    for i in range(0, num_rows):
+        if i == 0 or i == num_rows - 1:
+            print(ch * num_cols)
+        else:
+            print(ch + (" " * (num_cols - 2)) + ch)
+
