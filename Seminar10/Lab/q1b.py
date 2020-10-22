03a27856a5905f70ebfb425130f51c76cb4ed921
nixholas <nixholas@icloud.com>
2020-10-22 17:03:58 +0800

    Seminar10: Lab: q1b: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q1b.py
+++ Seminar10/Lab/q1b.py
@@ -1,7 +1,12 @@
 ### Q1b Reading Files
 # Write your code below:
+def print_alternate_columns(file_name):
+    file = open(file_name, 'r')
 
+    for line in file:
+        columns = line.rstrip().split('&')
 
-
-
-
+        line_content = ''
+        for i in range(0, len(columns), 2):
+            line_content += columns[i] + '&'
+        print(line_content[:-1])
