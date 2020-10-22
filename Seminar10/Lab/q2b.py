0bd463a1e7969bcdc75409f658aacc55e7064d63
nixholas <nixholas@icloud.com>
2020-10-22 17:13:58 +0800

    Seminar10: Lab: q2b: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q2b.py
+++ Seminar10/Lab/q2b.py
@@ -1,5 +1,14 @@
 ### Q2b: Universities 
 # Write your code below:
+def get_average_num_students(file_name):
+    file = open(file_name, 'r')
 
+    headcount = 0
+    school_count = 0
+    for line in file:
+        uni_datum = line.rstrip().split('\t')
+        school_count += 1
+        headcount += int(uni_datum[3])
 
-
+    # Return the xbar
+    return headcount/school_count
