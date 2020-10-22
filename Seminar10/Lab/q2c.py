8d1e70a46589ffb7f84421f1c6e5ab1e24a5860b
nixholas <nixholas@icloud.com>
2020-10-22 17:20:11 +0800

    Seminar10: Lab: q2c: DOne
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q2c.py
+++ Seminar10/Lab/q2c.py
@@ -1,5 +1,14 @@
 ### Q2c: Universities 
 # Write your code below:
+def search_with_keyword(file_name, keyword):
+    file = open(file_name, 'r')
+    acronyms = []
 
+    for line in file:
+        uni_datum = line.rstrip().split('\t')
 
+        if keyword.lower() in uni_datum[0].lower():
+            acronyms.append(uni_datum[1])
+
+    return acronyms
 
