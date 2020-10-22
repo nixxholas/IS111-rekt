602e2cf3f0e101f1572166a6203642476097ed7e
nixholas <nixholas@icloud.com>
2020-10-22 16:44:10 +0800

    Seminar10: Lab: q1a: doNE
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q1a.py
+++ Seminar10/Lab/q1a.py
@@ -2,8 +2,13 @@
 # Write your code below:
 ##############################
 
+def print_alternate_lines(file_name):
+    file = open(file_name, 'r')
 
-
-
-
-
+    is_odd_line = True
+    for line in file:
+        if is_odd_line:
+            print(line.rstrip())
+            is_odd_line = False
+        else:
+            is_odd_line = True
