3ee33931766be7a9c520f1d7460c61a57c1e6596
nixholas <nixholas@icloud.com>
2020-10-22 17:06:57 +0800

    Seminar10: Lab: q2a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q2a.py
+++ Seminar10/Lab/q2a.py
@@ -1,5 +1,14 @@
 ### Q2a: Universities 
 # Write your code below:
+def get_universities_founded_before(file_name, year):
+    file = open(file_name, 'r')
+    res = []
 
+    for line in file:
+        uni_datum = line.rstrip().split('\t')
 
+        if int(uni_datum[2]) < year:
+            res.append(uni_datum[0])
+
+    return res
 
