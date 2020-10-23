527375ea8af4bb0a92ce60d25264fe1831610157
nixholas <nixholas@icloud.com>
2020-10-24 01:15:31 +0800

    Seminar10: Lab: q5b: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q5b.py
+++ Seminar10/Lab/q5b.py
@@ -1,6 +1,18 @@
 ### Q5b: Matrix
+import copy
+from q5a import get_matrix
 
 # Write your code below:
+def get_matrix_transpose(file_name):
+    orig_matrix = get_matrix(file_name)
+    new_matrix = copy.deepcopy(orig_matrix)
+
+    # For every row in the matrix
+    for i in range(len(orig_matrix)):
+        # For every column in the row
+        for j in range(len(orig_matrix[i])):
+            # i.e. 0,0 becomes 0,0; 1,0 becomes 0,1
+            new_matrix[j][i] = orig_matrix[i][j]
 
 
-
+    return new_matrix
