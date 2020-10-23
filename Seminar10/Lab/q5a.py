789fe655fcdf451f5146cdaa739488a4b24e3981
nixholas <nixholas@icloud.com>
2020-10-23 19:51:53 +0800

    Seminar10: Lab: q5a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar10/Lab/q5a.py
+++ Seminar10/Lab/q5a.py
@@ -1,6 +1,17 @@
 ### Q5a: Matrix
 
 # Write your code below:
+def get_matrix(file_name):
+    file = open(file_name, 'r')
+    res = []
 
+    for line in file:
+        row = []
+        cells = line.rstrip().split(' ')
 
+        for cell in cells:
+            row.append(float(cell))
 
+        res.append(row)
+
+    return res
