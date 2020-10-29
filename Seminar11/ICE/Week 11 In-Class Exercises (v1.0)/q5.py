9be4039848f5a0e376d8d8f09bb51c86e9f80515
nixholas <nixholas@icloud.com>
2020-10-29 14:50:46 +0800

    Seminar11: ICE: q5: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

---
+++ Seminar11/ICE/Week 11 In-Class Exercises (v1.0)/q5.py
@@ -0,0 +1,14 @@
+with open('phone_numbers.txt') as phone_numbers_file:
+    contacts = {}
+    for line in phone_numbers_file:
+        cols = line.rstrip().split('|')
+        if cols[0] not in contacts:
+            contacts[cols[0]] = [cols[1]]
+        else:
+            contacts[cols[0]].append(cols[1])
+
+name = input("Enter a person’s name: ")
+if name in contacts:
+    print(name + ' has ' + str(len(contacts[name])) + ' number(s):')
+    for n in contacts[name]:
+        print('\t' + n)
