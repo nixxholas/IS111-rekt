5aaca40ce8cec873c1dc33cd8b46a907f68ad178
nixholas <nixholas@icloud.com>
2020-11-01 00:13:51 +0800

    Seminar11: ICE: YL: q1: DOne
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/ICE/YL/q1.py
+++ Seminar11/ICE/YL/q1.py
@@ -1,3 +1,3 @@
 # write your answer here
 def has_same_value(d1, d2, key):
-    return True if key in d1.keys() and key in d2.keys() else False
+    return True if (key in d1.keys() and key in d2.keys()) and (d1[key] == d2[key]) else False
