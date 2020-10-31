3edc7147178c91c6b86fb7bfcf5e3e3b13db5f65
nixholas <nixholas@icloud.com>
2020-11-01 00:18:06 +0800

    Seminar11: ICE: YL: q2: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/ICE/YL/q2.py
+++ Seminar11/ICE/YL/q2.py
@@ -1,1 +1,23 @@
 # write your answer here
+words = {}
+
+user_input = ' '
+while user_input != '':
+    user_input = input("Enter the word >")
+    if user_input != '':
+        if user_input in words.keys():
+            words[user_input] += 1
+        else:
+            words[user_input] = 1
+
+query_input = ' '
+while query_input != '':
+    query_input = input("Enter query word >")
+    if query_input != '':
+        if query_input in words:
+            print(words[query_input])
+            print()
+        else:
+            print("Not found.")
+
+print("Bye bye")
