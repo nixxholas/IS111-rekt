7142489e3b1296bfa5ac7bcd7471cb3aa77ae2a6
nixholas <nixholas@icloud.com>
2020-11-01 00:42:57 +0800

    Seminar11: ICE: YL: q5: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/ICE/YL/q5.py
+++ Seminar11/ICE/YL/q5.py
@@ -2,10 +2,17 @@
 def reverse(map):
     new_map = {}
 
+    # Loop the keys within the existing map
     for key in map.keys():
-        if map[key] in new_map.keys():
-            new_map[map[key]].append(key)
+        # Get the value of the key
+        value = map[key]
+        # If the value exists as a key in new_map,
+        if value in new_map.keys():
+            # It means there already contains a value.
+            # We push more into the value.
+            new_map[value].append(key)
         else:
-            new_map[map[key]] = [key]
+            # Else, we create a new key and value.
+            new_map[value] = [key]
 
     return new_map
