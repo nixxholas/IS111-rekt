e4ff01d7232ba009639c71e2c6e0c20e27a78e65
nixholas <nixholas@icloud.com>
2020-11-01 00:36:53 +0800

    Seminar11: ICE: YL: q5
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/ICE/YL/q5.py
+++ Seminar11/ICE/YL/q5.py
@@ -1,1 +1,11 @@
 # write your solution here
+def reverse(map):
+    new_map = {}
+
+    for key in map.keys():
+        if map[key] in new_map.keys():
+            new_map[map[key]].append(key)
+        else:
+            new_map[map[key]] = [key]
+
+    return new_map
