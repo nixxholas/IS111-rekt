c73713fd7b06df07daf6222196714c1502d59253
nixholas <nixholas@icloud.com>
2020-11-02 23:54:31 +0800

    Seminar11: Lab: q1a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/Lab/q1a.py
+++ Seminar11/Lab/q1a.py
@@ -1,3 +1,10 @@
-## Q1a Substitution Cipher
+# Q1a Substitution Cipher
 # write your code below
+def encrypt(my_dict, msg):
+    msg_c = list(msg)
+    for i in range(len(msg_c)):
+        if msg_c[i] in my_dict:
+            msg_c[i] = my_dict[msg[i]]
+
+    return "".join(msg_c)
 
