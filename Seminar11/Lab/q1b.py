1ff79050b2e8be260e306626b250aea79c7917ef
nixholas <nixholas@icloud.com>
2020-11-03 00:19:56 +0800

    Seminar11: Lab: q1b: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/Lab/q1b.py
+++ Seminar11/Lab/q1b.py
@@ -1,2 +1,11 @@
 ## Q1b Substitution Cipher
 # write your code below
+def decrypt(my_dict, encrypted_msg):
+    msg = list(encrypted_msg)
+
+    for i in range(len(msg)):
+        if msg[i] in my_dict:
+            msg[i] = my_dict[msg[i]]
+
+    return "".join(msg)
+
