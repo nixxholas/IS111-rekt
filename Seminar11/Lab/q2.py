3674dff2ba44e6bfb0881ed1adb3ee313123be2c
nixholas <nixholas@icloud.com>
2020-11-03 00:23:05 +0800

    Seminar11: Lab: q2: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar11/Lab/q2.py
+++ Seminar11/Lab/q2.py
@@ -1,2 +1,8 @@
 ## Q2 Prime Numbers
 # write your code below
+def create_prime_dict(arr):
+    res = {}
+    for val in arr:
+        res[val] = val <= 3 or (val > 3 and val % 2 != 0)
+
+    return res
