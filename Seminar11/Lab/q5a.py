1333a13e07ce6ceba216775a12d51a8102a01351
Nicholas Chen <nixholas@icloud.com>
2020-11-03 09:51:08 +0800

    Seminar11: Lab: q5a: Done!
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Seminar11/Lab/q5a.py
+++ Seminar11/Lab/q5a.py
@@ -1,2 +1,18 @@
 ## Q5a Word Counts
 # write your code below
+def count_words_in_file(file_name):
+    count_dict = {}
+
+    with open(file_name) as file:
+        # It's always a good practice to load line by line to prevent memory overflow.
+        for line in file:
+            line = line.rstrip().split(' ')
+            for word in line:
+                word = word.lower()
+                if word in count_dict:
+                    count_dict[word] += 1
+                else:
+                    count_dict[word] = 1
+
+    return count_dict
+
