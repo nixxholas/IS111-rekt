491edf43eda22bf0d6587f54944ccffcd1399ef2
Nicholas Chen <nixholas@icloud.com>
2020-11-03 10:45:48 +0800

    Seminar11: Lab: q6: Done!
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

---
+++ Seminar11/Lab/q6.py
@@ -0,0 +1,33 @@
+employees = {}
+
+with open('employees.txt') as file:
+    for line in file:
+        cols = line.rstrip().split('\t')
+        id = int(cols.pop(0).split(':')[1])
+
+        employee_details = {}
+        for i in range(len(cols)):
+            columns = cols[i].split(':')
+
+            employee_details[columns[0]] = int(columns[1]) if columns[1].isnumeric() else columns[1]
+
+        employees[id] = employee_details
+
+id = -1
+while id == -1 or input('Do you want to search for another employee? [Y|N]:') == 'Y':
+    id = int(input('Enter an employee ID: '))
+    if id in employees:
+        employee = employees[id]
+
+        print('This employee is ' + employee['name'] + '.')
+        field = ''
+        while field != 'S':
+            field = input("\tEnter a field name or 'S' to stop: ")
+
+            if field in employee:
+                print('\tThe ' + field + ' is ' + str(employee[field]))
+            else:
+                print('\tNot found!')
+    else:
+        print('Not found!')
+    print()
