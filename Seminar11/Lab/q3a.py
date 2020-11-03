1983dbc8668abc24f3ee16499b7f3617cb798eeb
Nicholas Chen <nixholas@icloud.com>
2020-11-03 09:07:27 +0800

    Seminar11: Lab: q3a: Done!
    Signed-off-by: Nicholas Chen <nixholas@icloud.com>

--- Seminar11/Lab/q3a.py
+++ Seminar11/Lab/q3a.py
@@ -4,9 +4,16 @@ facils = {}
 with open('facilities.txt') as facilities:
     for line in facilities:
         line = line.rstrip().split('\t')
-        facils[line[0]][line[1]] = (line[2], line[3])
+        facils[line[0], line[1]] = (line[2], line[3])
 
-while input('Do you want to search for a facility? [Y|N] :'):
-    sch = input('Enter the school [LKCSB|SOE|SOL|SOA|SIS] :')
-    rm_no = input('Enter the room number :')
-    print(line[0] + ' ' + line[1] + ' has a capacity of ' + line[2] + ' and does ' + ('' if line[3] else 'not ') + 'have a projector.')
+while input('Do you want to search for a facility? [Y|N] :') == 'Y':
+    room = (input('Enter the school [LKCSB|SOE|SOL|SOA|SIS] :'), input('Enter the room number :'))
+
+    if room in facils:
+        line = facils[room]
+        print(room[0] + ' ' + room[1] + ' has a capacity of ' + line[0] + ' and does ' + ('' if line[1] else 'not ')
+              + 'have a projector.')
+    else:
+        print('The specific room details were not found.')
+    print()
+
