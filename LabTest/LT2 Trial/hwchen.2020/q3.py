9b99eba678260babd4b8ddedfd26443827c1d0e1
nixholas <nixholas@icloud.com>
2020-11-05 08:59:51 +0800

    LabTest: LT2 Trial: q3: DOne
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 Trial/hwchen.2020/q3.py
+++ LabTest/LT2 Trial/hwchen.2020/q3.py
@@ -1,3 +1,42 @@
 def process_numbers(input_filename, output_filename):
     # Modify the code below.
+    with open(input_filename) as input_file:
+        incoming_data = input_file.readlines()
+
+    with open(output_filename, 'w') as output_file:
+        groups = 0
+        max_avg = 0
+        # Loop every line
+        for line in incoming_data:
+            # Since we're in the line, split the numbers by groups
+            line_groups = line.rstrip().split('#')
+            # Let the main output know how many groups there are in the line first
+            groups += len(line_groups)
+
+            # Print the result first
+            output_line = str(len(line_groups)) + ': '
+
+            # Loop every group
+            for i in range(len(line_groups)):
+                # Split all the numbers in the groups
+                numbers = line_groups[i].split(' ')
+                avg = 0
+
+                for number in numbers:
+                    try:
+                        avg += float(number)
+                    except ValueError:
+                        print('Non-float value detected and ignored.')
+
+                avg /= len(numbers)
+                if avg > max_avg:
+                    max_avg = avg
+                output_line += str(avg)
+                if i + 1 != len(line_groups):
+                    output_line += '#'
+
+            output_file.write(output_line + '\n')
+
+        output_file.write('Total number of groups: ' + str(groups) + '\n')
+        output_file.write('Maximum Average: ' + str(max_avg))
     pass
