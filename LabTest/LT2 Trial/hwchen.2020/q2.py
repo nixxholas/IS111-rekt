3b1f434436deacd7a9e75bb6fb9c4db6856dd725
nixholas <nixholas@icloud.com>
2020-11-05 08:33:40 +0800

    LabTest: LT2 Trial: q2: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 Trial/hwchen.2020/q2.py
+++ LabTest/LT2 Trial/hwchen.2020/q2.py
@@ -1,3 +1,10 @@
 def get_reversed_list(num_list):
+    # Flip first
+    num_list = num_list[::-1]
+
+    for i in range(len(num_list)):
+        if num_list[i] != 0:
+            num_list[i] *= -1
+
     # Modify the code below.
-    return
+    return num_list
