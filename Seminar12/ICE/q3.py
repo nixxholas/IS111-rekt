9ac4433682bfd3808a40f9c1fdde3374d6813ff3
nixholas <nixholas@icloud.com>
2020-11-05 13:00:23 +0800

    Seminar12: ICE: q3: Just to finalise things
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar12/ICE/q3.py
+++ Seminar12/ICE/q3.py
@@ -1,3 +1,4 @@
+# v2 - Legitimate
 def extract_bold_texts(html_text):
     res = []
     curr_str = ''
@@ -10,11 +11,25 @@ def extract_bold_texts(html_text):
             html_text = html_text[4:]
             res.append(curr_str)
             curr_str = ''
-        else:
+        # Just in case the last <b> has no closure.
+        elif len(html_text) > 3:
             html_text = html_text[1:]
+        else:
+            break
 
     return res
 
 
 print(extract_bold_texts('<b>ABC</b> abc <b>def 123 </b><b></b>0000'))
 print(extract_bold_texts('A piece of text without tags.'))
+
+# v1 method
+import re
+
+
+def extract_bold_texts_v1(html_text):
+    return re.findall("<b>(.*?)</b>", html_text)
+
+
+print(extract_bold_texts_v1('<b>ABC</b> abc <b>def 123 </b><b></b>0000'))
+print(extract_bold_texts_v1('A piece of text without tags.'))
