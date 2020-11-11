e24b59052051386b889b31320e4ddfe6ca2fa522
nixholas <nixholas@icloud.com>
2020-11-11 23:40:53 +0800

    LabTest: LT2 2019-2020: q3: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q3.py
+++ LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q3.py
@@ -1,6 +1,16 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def compute_total_price(price_dict, item_list):
     # Modify the code below.
-    return None
+    total = 0.0
+
+    for item in item_list:
+        item_name = item[0]
+        item_qty = item[1]
+
+        if item_name in price_dict.keys() and item_qty > 0:
+            total += (price_dict[item_name] * float(item_qty))
+
+    return total
+
