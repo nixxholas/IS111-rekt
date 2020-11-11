a84ff07749c3f10bca93dcc9444f7b7f146d965f
nixholas <nixholas@icloud.com>
2020-11-12 01:17:40 +0800

    LabTest: LT2 2019-2020: q4: Done!
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4b.py
+++ LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4b.py
@@ -16,10 +16,15 @@ def get_relation_through_link(family_dict, link):
             if (link[i], link[i + 1]) in family_dict.keys():
                 link_text.append(family_dict[(link[i], link[i + 1])])
 
-    rs_link = tuple(link_text)
     # Build the link_text as a tuple
-    if len(link_text) > 2:
-        front_link = (link_text[0], link_text[1])
-        rs_link = (relation_map[front_link], link_text[2])
+    while len(link_text) > 2:
+        # Take out the first two.
+        front_link = (link_text.pop(0), link_text.pop(0))
+        # Obtain the new rs for the first two
+        new_rs = relation_map[front_link]
+        # Replace the first with new_rs
+        link_text.insert(0, new_rs)
+
+    rs_link = tuple(link_text)
 
     return relation_map[rs_link]
--- LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4c.py
+++ LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4c.py
@@ -1,7 +1,35 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
+
+from q4b import get_relation_through_link
 
 def get_relation(family_dict, p1, p2):
     # Modify the code below.
-    return None
-    
+    current_pt = p1
+    cur_path = []
+    res = [p1]
+
+    banned_paths = []  # (B, C)
+    attempt = 0
+    while current_pt != p2:
+        if attempt > 1:
+            attempt = 0
+            current_pt = p1
+
+            if len(cur_path) == 1:
+                banned_paths.append((p1, cur_path[0]))
+            elif len(cur_path) > 1:
+                banned_paths.append((cur_path[len(cur_path) - 2], cur_path[len(cur_path) - 1]))
+            cur_path = []
+
+        for key in family_dict:
+            if key[0] == current_pt and key[1] != p1 and key not in banned_paths:
+                attempt = 0
+                cur_path.append(key[1])
+                current_pt = key[1]
+                if current_pt == p2:
+                    res += cur_path
+                    break
+        attempt += 1
+
+    return get_relation_through_link(family_dict, res)
