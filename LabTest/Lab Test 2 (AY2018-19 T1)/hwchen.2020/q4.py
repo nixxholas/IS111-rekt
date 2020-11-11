bb6b080906e35a3d3609739f5dd4e9e7eac9b205
nixholas <nixholas@icloud.com>
2020-11-11 16:04:03 +0800

    LabTest: LT2 2018-2019: q4: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 2 (AY2018-19 T1)/hwchen.2020/q4.py
+++ LabTest/Lab Test 2 (AY2018-19 T1)/hwchen.2020/q4.py
@@ -5,108 +5,57 @@
 
 # If needed, you can define your own additional functions here.
 # Start of your additional functions.
+def get_interchanges(mrt_map):
+    stations = {}
+    interchanges = {}
+    for line in mrt_map:
+        for stn in line:
+            if stn not in stations:
+                stations[stn] = mrt_map.index(line)
+            else:
+                interchanges[stn] = [stations[stn], mrt_map.index(line)]
+                del stations[stn]
+
+    return interchanges
+
+
+def get_station_coordinates(mrt_map, stn):
+    # Key = Line index, Value = Line's Station Index
+    # Line 0
+    # Line 1
+    # Line 2
+    coordinates = {}
+    for line in mrt_map:
+        if stn in line:
+            coordinates[mrt_map.index(line)] = line.index(stn)
+
+    return coordinates
 
 # End of your additional functions.
-def build_interchanges(mrt_map):
-    # Sample.
-    # (line1_index, line2_index, line1_stn_index, line2_stn_index, stn_name)
-    intersections = set()
-    # Loop every line once again
-    for line in mrt_map:
-        # Loop every station
-        for i in range(len(line)):
-            cur_stn = line[i]  # Set the current station just in case.
-
-            # Loop every other line:
-            for other_line in mrt_map:
-                # Make sure its not the line we're checking on and ensure that the cur_stn also exist in this
-                # other line.
-                if other_line is not line and cur_stn in other_line:
-                    # Since it is, its a cross section.
-                    intersections.add((mrt_map.index(line), mrt_map.index(other_line), i, other_line.index(cur_stn),
-                                       cur_stn))
-
-    return list(intersections)
-
-
-def get_distance_between_stations(mrt_map, s1, s2, interchanges):
-    s1_line_num = -1
-    s1_index = -1
-    s2_line_num = -1
-    s2_index = -1
-
-    for line in mrt_map:
-        # Basic flow
-        if s1 in line and s2 in line:
-            if line.index(s1) > line.index(s2):
-                return line.index(s1) - line.index(s2)
-            else:
-                return line.index(s2) - line.index(s1)
-        # Advanced flow, both stations are not on the station.
-        else:
-            if s1 in line:
-                s1_index = line.index(s1)
-                s1_line_num = mrt_map.index(line)
-            if s2 in line:
-                s2_index = line.index(s2)
-                s2_line_num = mrt_map.index(line)
-
-
-    return None
-
-
-def find_lines_in_s1_but_not_s2(mrt_map, s1, s2):
-    lines = []
-    for line in mrt_map:
-        if s1 in line and s2 not in line:
-            lines.append(line)
-    return lines
-
-
-def find_stations_within_distance(mrt_map, orig, dist):
+def find_stations_within_distance(mrt_map, orig, dist, parent=None):
     stations = set()
-    interchange_data = build_interchanges(mrt_map)
+    orig_coords = get_station_coordinates(mrt_map, orig)
+    interchanges = get_interchanges(mrt_map)
+    parent = orig if parent is None else parent
 
-    for line in mrt_map:
-        for station in line:
-            if orig != station:
-                dist = get_distance_between_stations(mrt_map, orig, station, interchange_data)
+    # Iterate every coordinate existing in orig_coords
+    for line_id in orig_coords.keys():
+        # Get orig's index for this line first
+        orig_index = mrt_map[line_id].index(orig)
 
-    # Loop the lines, get the interchange data first.
-    # for line in mrt_map:
-    #     # Ensure the origin is in the line.
-    #     if orig in line:
-    #         # Get the index of the orig if its in this line
-    #         orig_i = line.index(orig)
-    #
-    #         # Loop the layers
-    #         for i in range(dist):
-    #             # Perform Naive Range Traversal First
-    #             if orig_i - (i + 1) >= 0:
-    #                 stations.add(line[orig_i - (i + 1)])
-    #             if orig_i + (i + 1) < len(line):
-    #                 stations.add(line[orig_i + (i + 1)])
-    #
-    # # Now we, need to ensure that we have inter-line checks as well.
-    # if dist > 1:
-    #     stations_to_track = list(stations)
-    #     # Traverse every station.
-    #     for station in stations_to_track:
-    #         stn_dist = get_distance_between_stations(mrt_map, orig, station)
-    #         cur_dist_left = dist - stn_dist
-    #
-    #         if cur_dist_left > 0:
-    #             lines_to_search = find_lines_in_s1_but_not_s2(mrt_map, station, orig)
-    #
-    #             for line in lines_to_search:
-    #                 stn_index = line.index(station)
-    #
-    #                 # Loop the layers
-    #                 for i in range(cur_dist_left):
-    #                     # Perform Naive Range Traversal First
-    #                     if stn_index - (i + 1) >= 0:
-    #                         stations.add(line[orig_i - (i + 1)])
-    #                     if stn_index + (i + 1) < len(line):
-    #                         stations.add(line[orig_i + (i + 1)])
+        # Start travelling
+        for station in mrt_map[line_id]:  # Loop every station first.
+            # Ensure we're not double checking the same station.
+            if station != orig and station != parent and station not in stations:
+                # Get the index.
+                stn_index = mrt_map[line_id].index(station)
+                difference = stn_index - orig_index if orig_index < stn_index else orig_index - stn_index
+
+                if difference <= dist:
+                    stations.add(station)
+
+                # If the station being checked is an interchange and if there's still more difference.
+                if station in interchanges.keys() and dist > difference:
+                    stations = stations | find_stations_within_distance(mrt_map, station, dist - difference, parent)
 
     return stations
