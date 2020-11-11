04756abee7926466cf5e909d1fef32fba39f8da1
nixholas <nixholas@icloud.com>
2020-11-11 23:32:14 +0800

    LabTest: LT2 2018-2019: q5: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 2 (AY2018-19 T1)/hwchen.2020/q5.py
+++ LabTest/Lab Test 2 (AY2018-19 T1)/hwchen.2020/q5.py
@@ -3,33 +3,39 @@
 # Email ID: hwchen.2020
 #
 
+import copy
+
 # If needed, you can define your own additional functions here.
 # Start of your additional functions.
-def generate_child(num_list_str):
+def str_list_traversal(num_list_str):
     res = []
 
-    return res
+    cur_val = ''
+    while len(num_list_str) > 0:
+        if num_list_str[0] == '[':
+            res.append(str_list_traversal(num_list_str[1:]))
+            # Slice the string and take out the length of the current res - the whitespaces
+            num_list_str = num_list_str[len(str(res[len(res) - 1])) - (len(res[len(res) - 1]) - 1):]
+        elif num_list_str[0] == ']':
+            if cur_val.isnumeric():
+                res.append(int(cur_val))
+            cur_val = ''
+            num_list_str = num_list_str[1:]
+            return res
+        elif num_list_str[0] == ',':
+            if cur_val.isnumeric():
+                res.append(int(cur_val))
+            cur_val = ''
+            num_list_str = num_list_str[1:]
+        else:
+            cur_val += copy.deepcopy(num_list_str[0])
+            num_list_str = num_list_str[1:]
 
+    return res
 # End of your additional functions.
 
 def convert_to_list(num_list_str):
-    res = []
-    num_list_str = num_list_str[1:len(num_list_str) - 1]
+    res = str_list_traversal(num_list_str)
 
-    # Always loop till we clear the string
-    while len(num_list_str) > 0:
-        # Take the first item first
-        cur_item = num_list_str.split(',')[0]
-
-        # If it's just the value already
-        if cur_item.isnumeric():
-            # Easy game
-            res.append(cur_item)
-            # Update the string
-            num_list_str = num_list_str[len(cur_item) + 1:]
-        # Else it's a list...
-        else:
-            res.append(generate_child(num_list_str))
-
-    return res
+    return res[0]
 
