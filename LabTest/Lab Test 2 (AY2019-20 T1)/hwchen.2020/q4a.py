10092dab46a328418de07d0171e5125f7af743e4
nixholas <nixholas@icloud.com>
2020-11-11 23:59:24 +0800

    LabTest: LT2 2019-2020: q4a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4a.py
+++ LabTest/Lab Test 2 (AY2019-20 T1)/hwchen.2020/q4a.py
@@ -1,7 +1,28 @@
-# Name:
-# Email ID:
+# Name: Nicholas Chen Han Wei
+# Email ID: hwchen.2020
 
 def store_family_relations(family_file):
+    res = {}
     # Modify the code below.
-    return None
-    
+    with open(family_file) as families:
+        for family in families:
+            parents_data = family.split(':')[0]
+            parents = parents_data[1:len(parents_data) - 1].split(',')
+            children = family.split(':')[1].split(';')
+
+            for i in range(len(parents)):
+                rs = 'father' if i == 0 else 'mother'
+                for child in children:
+                    child_detail = child[1:len(child) - 1].split(',')
+                    name = child_detail[0]
+                    res[(parents[i], name)] = rs
+
+            for child in children:
+                child_detail = child[1:len(child) - 1].split(',')
+                name = child_detail[0]
+                gender = child_detail[1]
+                for parent in parents:
+                    res[(name, parent)] = 'daughter' if 'F' in gender else 'son'
+
+    return res
+
