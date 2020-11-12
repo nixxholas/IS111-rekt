b3144b96f0d1844898f481a2cac5ea1e242f3bf3
nixholas <nixholas@icloud.com>
2020-11-12 10:06:52 +0800

    LabTest: LT2 Trial 2: Some optimisations
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 Trial - _v2/hwchen.2020/q2.py
+++ LabTest/LT2 Trial - _v2/hwchen.2020/q2.py
@@ -1,10 +1,13 @@
 # Write your code below:
 alias = '@smu.edu.sg'
 email = ''
-while len(email) <= len(alias) or not email.endswith(alias) or '@' in email[:len(email) - len(alias)]:
+while len(email) <= len(alias) or not email.endswith(alias) or len(email[:len(email) - len(alias)]) < 0 \
+        or '@' in email[:len(email) - len(alias)]:
     email = input('Please enter your SMU email address:')
 
-    if len(email) <= len(alias) or not email.endswith(alias) or '@' in email[:len(email) - len(alias)]:
+    if len(email) <= len(alias) or not email.endswith(alias) or len(email[:len(email) - len(alias)]) < 0 \
+        or '@' in email[:len(email) - len(alias)]:
         print('Invalid!')
 
 print('Thanks!')
+
