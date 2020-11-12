7bd9ea6b717f0debc7ae12dd7717841019b8855c
nixholas <nixholas@icloud.com>
2020-11-12 08:57:23 +0800

    LabTest: LT2 Trial 2: q2: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 Trial - _v2/hwchen.2020/q2.py
+++ LabTest/LT2 Trial - _v2/hwchen.2020/q2.py
@@ -1,1 +1,10 @@
 # Write your code below:
+alias = '@smu.edu.sg'
+email = ''
+while len(email) <= len(alias) or not email.endswith(alias) or '@' in email[:len(email) - len(alias)]:
+    email = input('Please enter your SMU email address:')
+
+    if len(email) <= len(alias) or not email.endswith(alias) or '@' in email[:len(email) - len(alias)]:
+        print('Invalid!')
+
+print('Thanks!')
