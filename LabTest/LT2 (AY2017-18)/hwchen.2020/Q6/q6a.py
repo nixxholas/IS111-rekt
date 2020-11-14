3d28fca8fee0c4500ab12898339ad0d61de5a7cc
nixholas <nixholas@icloud.com>
2020-11-15 01:10:01 +0800

    LabTest: LT2 2017-2018: q6a: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 (AY2017-18)/hwchen.2020/Q6/q6a.py
+++ LabTest/LT2 (AY2017-18)/hwchen.2020/Q6/q6a.py
@@ -1,4 +1,24 @@
 def scramble(word, file_name):
-    # modify the code below
-    return []
-    
+    res = []
+    common_words = []
+    with open(file_name) as document:
+        for line in document:
+            common_words.append(line.rstrip())
+
+    for common_word in common_words:
+        if is_similar(word, common_word):
+            res.append(common_word)
+    return res
+
+
+def is_similar(word, other_word):
+    word_chars = list(word)
+    other_word_chars = list(other_word)
+    if len(word_chars) == 0 or len(other_word_chars) == 0 or len(word_chars) != len(other_word_chars):
+        return False
+
+    for c in word_chars:
+        if c in other_word_chars:
+            other_word_chars.remove(c)
+
+    return len(other_word_chars) == 0
