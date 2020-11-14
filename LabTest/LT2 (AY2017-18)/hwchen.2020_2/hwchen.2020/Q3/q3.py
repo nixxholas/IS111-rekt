bedf8df5818f35c59247dc0fa86054288e590c6e
nixholas <nixholas@icloud.com>
2020-11-14 15:00:22 +0800

    LabTest: LT 2017-2018: hwchen.2020_2: set 2 dupe
    Signed-off-by: nixholas <nixholas@icloud.com>

--- LabTest/LT2 (AY2017-18)/hwchen.2020_2/hwchen.2020/Q3/q3.py
+++ LabTest/LT2 (AY2017-18)/hwchen.2020_2/hwchen.2020/Q3/q3.py
@@ -1,3 +1,8 @@
 def get_postal_codes(file_name):
     # modify the code below
+    with open(file_name) as dataset:
+        t_list = []
+        for line in dataset:
+            t_list.append(line)
+        print()
     return []
