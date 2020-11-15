c363417417d437d1c8ae131b85c83aad4aff8224
nixholas <nixholas@icloud.com>
2020-11-15 17:54:52 +0800

    Seminar12: LYL: q3b: DOne
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar12/LYL/Trial2/Q3/q3b.py
+++ Seminar12/LYL/Trial2/Q3/q3b.py
@@ -1,2 +1,8 @@
 def add_new_email(email_dict, tuple):
-    return None
+    if tuple[0] in email_dict.keys():
+        old_res = email_dict[tuple[0]] + ''
+        email_dict[tuple[0]] = tuple[1]
+        return old_res
+    else:
+        email_dict[tuple[0]] = tuple[1]
+        return None
