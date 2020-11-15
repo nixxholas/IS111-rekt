f4bd6c0cb30b3f65ad66d420e57c5986e8a87b95
nixholas <nixholas@icloud.com>
2020-11-15 18:01:02 +0800

    Seminar12: LYL: Trial2: q3b: done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar12/LYL/Trial2/Q3/q3b.py
+++ Seminar12/LYL/Trial2/Q3/q3b.py
@@ -1,6 +1,6 @@
 def add_new_email(email_dict, tuple):
     if tuple[0] in email_dict.keys():
-        old_res = email_dict[tuple[0]] + ''
+        old_res = email_dict[tuple[0]]  # This is deep copy
         email_dict[tuple[0]] = tuple[1]
         return old_res
     else:
