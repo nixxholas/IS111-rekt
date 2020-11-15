a6f9771a93326136b4b553d9a69708f0fbef0120
nixholas <nixholas@icloud.com>
2020-11-15 17:43:36 +0800

    Seminar12: LYL: Trial2: Q2: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar12/LYL/Trial2/Q2/q2.py
+++ Seminar12/LYL/Trial2/Q2/q2.py
@@ -1,1 +1,9 @@
 # Write your code below:
+while True:
+    email = input('Please enter your SMU email address:')
+
+    if len(email.split('@')) != 2 or len(email.split('@')[0]) == 0 or len(
+            email.split('@')[1]) == 0 or not email.endswith('smu.edu.sg'):
+        print('Invalid!')
+    else:
+        break
