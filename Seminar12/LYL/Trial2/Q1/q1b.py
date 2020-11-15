f27f11259b74e1f20355fcc6dab664e4de490c31
nixholas <nixholas@icloud.com>
2020-11-15 17:36:46 +0800

    Seminar12: LYL: Trial2: q1b: Done
    Signed-off-by: nixholas <nixholas@icloud.com>

--- Seminar12/LYL/Trial2/Q1/q1b.py
+++ Seminar12/LYL/Trial2/Q1/q1b.py
@@ -1,2 +1,5 @@
 def create_email_dict(info_list):
-    return {}
+    email_dict = {}
+    for i in range(0, len(info_list), 2):
+        email_dict[info_list[i]] = info_list[i + 1]
+    return email_dict
